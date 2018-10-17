030f32fb31ea0060bfbf875bed43c1869ef66ab7
Youp <youpkuiper@gmail.com>
2018-10-17 10:37:19 +0200

    Add slottype first, if it already exists the database entry wont go through

--- SimonSaysResourceEditor.py
+++ SimonSaysResourceEditor.py
@@ -1,27 +1,15 @@
-# Use this script to add templates for resources to the database and add slottypes to lex
 import sys
 import json
 import boto3
 from dbhandler import dbhandler
-
-# Get dynamodb table
-table = dbhandler.getDB()
-
-# Read file from json and parse to dict
-jsonFile = open(sys.argv[1], "r")
-input = jsonFile.read()
-inputDict = json.loads(input)
-
-# Get the type of resource
-type = sys.argv[2]
-
-# Put the template in the table
-response = table.put_item(Item={"Type": type, "json": inputDict})
-print(response)
+# Use this script to add templates for resources to the database and add slottypes to lex
 # Add type to lex slotTypes
 
 # Get the lex client
 client = boto3.client('lex-models')
+
+# Get the type of resource
+type = sys.argv[2]
 
 response = client.get_slot_type(
     name='Resources',
@@ -41,5 +29,15 @@ response = client.put_slot_type(
     valueSelectionStrategy='ORIGINAL_VALUE',
     createVersion=False
 )
+# Get dynamodb table
+table = dbhandler.getDB()
 
-# print(response)
+# Read file from json and parse to dict
+jsonFile = open(sys.argv[1], "r")
+input = jsonFile.read()
+inputDict = json.loads(input)
+
+# Put the template in the table
+response = table.put_item(Item={"Type": type, "json": inputDict})
+
+print(response)
