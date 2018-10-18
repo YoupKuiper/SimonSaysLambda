696aff7d3024621cc0a2ef520c9519da2dc896a9
Sven Preng <sven_preng@live.nl>
2018-10-18 09:05:37 +0200

    Added new functionality to lambda function including deployProject Added proper feedback about invalid resources

--- lambda_function.py
+++ lambda_function.py
@@ -7,6 +7,7 @@ from dbhandler import dbhandler
 
 cloudFormationClient = boto3.client('cloudformation')
 lexBotClient = boto3.client('lex-models')
+t = TemplateBuilder()
 
 projTable = dbhandler.getDB("project")
 
@@ -17,7 +18,9 @@ def lambda_handler(event, context):
     if currentIntent == "CreateProject":
         return createProject(event)
     elif currentIntent == "AddResources":
-        return buildTemplate(event)
+        return addResourcesToProject(event)
+    elif currentIntent == "DeployProject":
+        return deployProject(event)
     else:
         return buildLexResponse(1, "Error, unrecognized intent", None, None)
 
@@ -39,6 +42,8 @@ def buildLexResponse(error, message, sessionAttributesToAppend, event):
         }
     }
 
+
+# Create a list of allowed resources
 def getAllowedResources():
     allowedresources = []
     response = lexBotClient.get_slot_type(
@@ -49,6 +54,8 @@ def getAllowedResources():
         allowedresources = allowedresources + (value['synonyms'])
     return allowedresources
 
+
+# Use the given name as the name for the project
 def createProject(event):
     projectName = event['currentIntent']['slots']['ProjectName']
     message = f"Project {projectName} has been created, please define the resources you want to have in your project"
@@ -58,30 +65,62 @@ def createProject(event):
     return buildLexResponse(0, message, sessionAttributesToAppend, event)
 
 
-def buildTemplate(event):
-    t = TemplateBuilder()
-    projectName = event['sessionAttributes']['projectName']
-    strtest = ""
-    resources = event['currentIntent']['slots']
-    for resourceSlot in resources:
-        resource = resources[resourceSlot]
+# Create lists of both valid and invalid resources
+def validateResources(resourcesToValidate):
+    valid = []
+    invalid = []
+    for resourceSlot in resourcesToValidate:
+        resource = resourcesToValidate[resourceSlot]
         if resource in getAllowedResources():
+            valid.append(resource)
+        elif resource is not None:
+            invalid.append(resource)
+    return valid, invalid
+
+
+def listResponseBuilder(list):
+    listresponse = ""
+    for resource in list:
+        if list.index(resource) is (len(list) - 1) and (len(list)) != 1:
+            listresponse = listresponse + " and " + resource
+        elif list.index(resource) is 0:
+            listresponse = listresponse + resource
+        else:
+            listresponse = listresponse + ", " + resource
+    return listresponse
+
+
+def addResourcesToProject(event):
+    resources = event['currentIntent']['slots']
+    source = event['invocationSource']
+    if source == 'DialogCodeHook':
+        valid, invalid = validateResources(resources)
+        if len(invalid) == 0:
+            validResourceString = listResponseBuilder(valid)
+            invalidResourceString = listResponseBuilder(invalid)
+            if len(valid) == 1:
+                messageValid = f"The resource: {validResourceString} is valid."
+            else:
+                messageValid = f"The resources: {validResourceString} are valid."
+            if len(invalid) == 1:
+                messageInvalid = f"The resource: {invalidResourceString} is invalid, please restate it."
+            else:
+                messageInvalid = f"The resources: {invalidResourceString} were invalid, please restate them."
+                message = messageValid + " " + messageInvalid
+            return buildLexResponse(0, message, {}, event)
+        projectName = event['sessionAttributes']['projectName']
+        valid, invalid = validateResources(resources)
+        for resource in valid:
             t.addResource(resource)
-        elif resource is None:
-            print("None")
-        else:
-            return buildLexResponse(1, f"Resource: {resource} not recognized",[],event)
-        if list(resources).index(resourceSlot) is (len(resources) - 1):
-            strtest = strtest + " and " + resource
-        elif list(resources).index(resourceSlot) is 0:
-            strtest = strtest + resource
-        else:
-            strtest = strtest + ", " + resource
-    projTable.put_item(Item={"ProjectName": projectName, "resources": list(resources.values())})
-    #createStackFromTemplateBody(projectName, t.getTemplate())
-    message = f"The resources: {strtest} were added to project {projectName}."
-    sessionAttributesToAppend = {}
-    return buildLexResponse(0, message, sessionAttributesToAppend, event)
+        projTable.put_item(Item={"ProjectName": projectName, "resources": list(resources.values())})
+        sessionAttributesToAppend = {}
+        message = "Resources added to project"
+        return buildLexResponse(0, message, sessionAttributesToAppend, event)
+
+def deployProject(event):
+    projectName = event['sessionAttributes']['projectName']
+    createStackFromTemplateBody(projectName, t.getTemplate())
+    return buildLexResponse(0, f"Deployed {projectName}", {}, event)
 
 
 def appendSessionAttributes(attributes, attributesToAppend):
