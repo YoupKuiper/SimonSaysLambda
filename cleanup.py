3c77c4ed218a2b494619e6dc1acd859e41aa86d0
Sven Preng <sven_preng@live.nl>
2018-11-02 15:42:45 +0100

    Removed unnecessary script

--- cleanup.py
+++
@@ -1,9 +0,0 @@
-import boto3, os, shutil
-
-cloudFormationClient = boto3.client('cloudformation')
-
-stackName = 'teststack'
-
-response = cloudFormationClient.delete_stack(
-        StackName=stackName,
-)
