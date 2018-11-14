94c30da75e5f135e3e914302ce5e04fd0d3940f6
Sven Preng <sven_preng@live.nl>
2018-11-14 14:51:44 +0100

    Added apigateway with custom domainname

--- pipelineTestScript.py
+++ pipelineTestScript.py
@@ -19,7 +19,7 @@ projectName = "TestProject"
 stackName = sys.argv[1]
 
 jsonFile = open(
-    "/Users/spreng/development/SimonSaysLambda/templates/new/test.json")
+    "/Users/spreng/development/SimonSaysLambda/templates/newWebsite/APIGateway.json")
 userInput = jsonFile.read()
 
 print("Creating stack...")
@@ -40,41 +40,10 @@ while response['Stacks'][0]['StackStatus'] == "CREATE_IN_PROGRESS":
 
 print("Stack created...")
 
-print("Creating repo...")
-GIT_SSH_KEY = os.path.expanduser('~/.ssh/id_rsa')
-git_ssh_cmd = 'ssh -i %s' % GIT_SSH_KEY
-
-with Git().custom_environment(GIT_SSH_COMMAND=git_ssh_cmd):
-    newrepo = Repo.clone_from(
-        "ssh://git-codecommit.eu-west-1.amazonaws.com/v1/repos/TestProjectRepo",
-        "/Users/spreng/development/newrepo")
-
-print("Repo created...")
-
-print("Getting necessary files...")
-
-s3.Bucket(BUCKET_NAME).download_file("test.zip", "test.zip")
-
-print("Files downloaded")
-
-zip_ref = zipfile.ZipFile('test.zip', 'r')
-zip_ref.extractall("/Users/spreng/development/newrepo/")
-zip_ref.close()
-
-newrepo.git.add("/Users/spreng/development/newrepo/*")
-shutil.copyfile("/Users/spreng/development/SimonSaysLambda/index.js","/Users/spreng/development/newrepo/index.js")
-newrepo.git.add("/Users/spreng/development/newrepo/index.js")
-newrepo.git.commit('-m', 'Test using python script')
-newrepo.git.push()
 
 input("Press Enter to delete stack")
 
-print("Deleting the created stack and repo")
+print("Deleting the created stack")
 
 response = cloudFormationClient.delete_stack(
         StackName=stackName)
-response = cloudFormationClient.delete_stack(
-        StackName="TestProjectStack"
-)
-
-shutil.rmtree('/Users/spreng/development/newrepo/')
