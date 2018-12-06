59d1844e7f3d4dbe87d599d9782dd39b1d0b3a76
Youp <youpkuiper@gmail.com>
2018-12-06 13:39:18 +0100

    added python 3.6 syntax

--- Microservices/copy_s3_objects/index.py
+++ Microservices/copy_s3_objects/index.py
@@ -25,7 +25,8 @@ def handler(event, context):
     if event['RequestType'] == 'Create' or event['RequestType'] == 'Update':
       result = copy_objects(source_bucket, source_prefix, bucket, prefix, APIUrl)
     elif event['RequestType'] == 'Delete':
-      result = lambdaClient.invoke(FunctionName='S3BucketDeletionService', InvocationType='RequestResponse', Payload=bucket)
+      print('deleting')
+      result = lambdaClient.invoke(FunctionName='S3BucketDeletionService', InvocationType='RequestResponse', Payload=json.dumps(event['ResourceProperties']))
   except ClientError as e:
     logger.error('Error: %s', e)
     result = cfnresponse.FAILED
@@ -44,6 +45,6 @@ def copy_objects(source_bucket, source_prefix, bucket, prefix, APIUrl):
   for key in {x['Key'] for page in page_iterator for x in page['Contents']}:
     dest_key = os.path.join(prefix, os.path.relpath(key, source_prefix))
     if not key.endswith('/'):
-      print 'copy {} to {}'.format(key, dest_key)
+      print('copy {} to {}'.format(key, dest_key))
       client.copy_object(CopySource={'Bucket': source_bucket, 'Key': key}, Bucket=bucket, Key = dest_key)
   return cfnresponse.SUCCESS
