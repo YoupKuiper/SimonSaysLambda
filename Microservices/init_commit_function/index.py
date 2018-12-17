ba6fc156d7f1a0787af2322741c0d2e976aa93a7
Sven Preng <sven_preng@live.nl>
2018-12-17 09:41:58 +0100

    Added permissions to the setup_simonsays template

--- Microservices/init_commit_function/index.py
+++ Microservices/init_commit_function/index.py
@@ -1,77 +1,83 @@
-import boto3
-import os
-import zipfile
-import cfnresponse
 import json
-import yaml
+import os
+import cfnresponse
+import boto3
+import zipfile
 
-
-FILEDIR = "/tmp/files/"
 ROOTDIR = "/tmp/"
-s3Client = boto3.resource('s3')
-codecommitClient = boto3.client('codecommit')
 
 
 def lambda_handler(event, context):
-    if event['RequestType'] == 'Delete':
-        result = cfnresponse.SUCCESS
-        print("Deleting")
-        return cfnresponse.send(event, context, result, {})
 
+    # Setup clients for services
+    lexClient = boto3.client('lex-models')
+    s3Client = boto3.resource('s3')
     result = cfnresponse.FAILED
 
-    #Download zip to lambda
-    try:
+    # If the cfn request is a delete, delete the created bot
+    if event["RequestType"] == "Delete":
+        response = lexClient.delete_bot(
+            name="SimonSays"
+                )
+        result = cfnresponse.SUCCESS
+        return cfnresponse.send(event, context, result, {})
+    # In the case of an update, there is nothing to do
+    elif event["RequestType"] == "Update":
+        result = cfnresponse.SUCCESS
+        return cfnresponse.send(event, context, result, {})
+    # In the case of a create setup the required variables
+    else:
         bucketName = event["ResourceProperties"]["BucketName"]
         contentZipName = event["ResourceProperties"]["ContentZipName"]
-        projectName = event["ResourceProperties"]["ProjectName"]
+        lambdaARN = event["ResourceProperties"]["LambdaARN"]
+
+        # Download the file
         os.chdir(ROOTDIR)
-        s3Client.Bucket(bucketName).download_file(contentZipName, contentZipName)
-    except Exception as e:
-        print(e)
-        return cfnresponse.send(event, context, result, {})
+        try:
+            s3Client.Bucket(bucketName).download_file(contentZipName, contentZipName)
+        except Exception as e:
+            print(e)
+            return cfnresponse.send(event, context, result, {})
 
-    #Unpack the zipfile
-    try:
-        zip_ref = zipfile.ZipFile(contentZipName, 'r')
-        zip_ref.extractall(FILEDIR)
-        zip_ref.close()
-    except Exception as e:
-        print(e)
-        return cfnresponse.send(event, context, result, {})
+        try:
+            templatefile = open("/tmp/" + contentZipName, "r")
+            template = templatefile.read()
+            templateDict = json.loads(template)
+            # Edit the proper Lambda ARN into the template
+            for intent in templateDict["resource"]["intents"]:
+                if "fulfillmentActivity" in intent:
+                    fulfillmentActivity = intent["fulfillmentActivity"]
+                    if "codeHook" in fulfillmentActivity:
+                        codeHook = fulfillmentActivity["codeHook"]
+                        codeHook["uri"] = lambdaARN
+            print(templateDict)
+        except Exception as e:
+            print(e)
+            return cfnresponse.send(event, context, result, {})
 
+        try:
+            zf = zipfile.ZipFile('zipfile_write.zip', mode='w')
+            zf.write(contentZipName, json.dumps(templateDict))
+            zf.close()
+        except Exception as e:
+            print(e)
+            return cfnresponse.send(event, context, result, {})
 
-    #Create initial commit
-    try:
-        repoName = event["ResourceProperties"]["RepositoryName"]
-        readmeContent = 'This repository is provisioned by AWS lambda'
-        initialCommit = codecommitClient.put_file(repositoryName=repoName, branchName='master', fileContent=readmeContent, filePath='readme.md')
-        commitId = initialCommit['commitId']
-    except Exception as e:
-        print(e)
-        return cfnresponse.send(event, context, result, {})
+        try:
+            zipbot = open("/tmp/" + "zipfile_write.zip", "rb")
+            zipbotdata = zipbot.read()
+            response = lexClient.start_import(
+                payload=zipbotdata,
+                resourceType='BOT',
+                mergeStrategy='OVERWRITE_LATEST'
+                )
 
+            while(response['importStatus'] == 'IN_PROGRESS'):
+                response = lexClient.get_import(importId=response['importId'])
 
-    #Write unpacked zip to codeCommit
-    try:
-        for filename in os.listdir(FILEDIR):
-            in_file = open("/tmp/files/" + filename, "rb")
-            data = in_file.read()
-            if filename == "template.json":
-                datajson = json.loads(data)
-                datajson["Parameters"]["ProjectName"]["Default"] = projectName
-                data = json.dumps(datajson, sort_keys=False, indent=4, separators=(',', ':'))
-            elif filename == "buildspec.yml":
-                yamltojson = yaml.load(data)
-                yamltojson['phases']['post_build']['commands'][0] = yamltojson['phases']['post_build']['commands'][0].replace("PIPELINE_BUCKET", "codepipeline-" + projectName + "-artifactbucket")
-                data = yaml.dump(yamltojson)
-            in_file.close()
-            response = codecommitClient.put_file(repositoryName=repoName, branchName='master', fileContent=data ,filePath=filename, parentCommitId=commitId)
-            commitId = response['commitId']
-    except Exception as e:
-        print(e)
-        return cfnresponse.send(event, context, result, {})
+            result = cfnresponse.SUCCESS
+        except Exception as e:
+            print(e)
+            return cfnresponse.send(event, context, result, {})
 
-
-    result = cfnresponse.SUCCESS
     cfnresponse.send(event, context, result, {})
